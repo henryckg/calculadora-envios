02cafb8cffb79128668000b652dc574b96cdeea8
henryck <guaramatohenryck@gmail.com>
2023-07-10 22:50:37 -0400

    correciones

--- js/index.js
+++ js/index.js
@@ -1,29 +1,36 @@
 // Calculadora de envíos
 
+//Declaro variable global
+let resultadoEnvio
+
+
 //Funciones
 
-function calcularEnvio(tarifaEnvio){
-    
-    //Se solicitan dimensiones y peso del paquete
-    let alto = parseFloat(prompt("Ingrese el alto del paquete (cm)"))
-    let ancho = parseFloat(prompt("Ingrese el ancho del paquete (cm)"))
-    let largo = parseFloat(prompt("Ingrese el largo del paquete (cm)"))
-    let peso = parseFloat(prompt("Ingrese el peso del paquete (kg)"))
+function calcularEnvio(tarifaEnvio){    
 
-    let pesoVolumetrico = (alto * ancho * largo) / 5000
+    do{
+        //Se solicitan dimensiones y peso del paquete
+        let alto = parseFloat(prompt("Ingrese el alto del paquete (cm)"))
+        let ancho = parseFloat(prompt("Ingrese el ancho del paquete (cm)"))
+        let largo = parseFloat(prompt("Ingrese el largo del paquete (cm)"))
+        let peso = parseFloat(prompt("Ingrese el peso del paquete (kg)"))
 
-    //Para el cáculo de envío se toma en cuenta el número mayor entre peso y peso volumétrico
-    if (peso >= pesoVolumetrico){
-        return resultado = peso * parseFloat(tarifaEnvio)
-    } else if (peso < pesoVolumetrico){
-        return resultado = pesoVolumetrico * tarifaEnvio
-    } else{
-        alert("Hay alguna medida que no corresonde a un valor númerico. Reintente")
-    }
+        let pesoVolumetrico = (alto * ancho * largo) / 5000
+
+        //Para el cáculo de envío se toma en cuenta el número mayor entre peso y peso volumétrico
+        if (peso >= pesoVolumetrico){
+            resultadoEnvio = peso * parseFloat(tarifaEnvio)
+        } else if (peso < pesoVolumetrico){
+            resultadoEnvio = pesoVolumetrico * tarifaEnvio
+        } else{
+            alert("Hay alguna medida que no corresonde a un valor númerico. Haga la operación nuevamente")
+        }
+    } while (isNaN(resultadoEnvio))
 }
 
 const suma = function (a, b) { return a + b}
 const seguro = function (x) { return x * 10/100 }
+
 
 //Inicio del simulador
 
@@ -57,15 +64,18 @@ switch (tipoEnvio) {
         break;
 }
 
+
+//Opción para asegurar el envío
+
 let asegurar = prompt("Deseas asegurar el envío?. \nIngrese: Y ó N")
 
 if (asegurar == "Y" || asegurar == "y"){   
     let precioProducto = parseInt(prompt("Ingrese el valor del producto en USD"))
-    let nuevoPrecio = suma(resultado, seguro(precioProducto))
-    alert("El valor del envío con seguro es USD " + nuevoPrecio + "\n(Monto sin seguro: USD " + resultado + ")")
+    let nuevoPrecio = suma(resultadoEnvio, seguro(precioProducto))
+    alert("El valor del envío, incluyendo el seguro, es: USD " + nuevoPrecio + "\n(Monto sin seguro: USD " + resultadoEnvio + ")")
     
 } else if (asegurar == "N" || asegurar == "n"){
-    alert("El valor del envío es USD " + resultado + ". (Sin seguro)")
+    alert("El valor del envío es: USD " + resultadoEnvio + ". (Sin seguro)")
 } else{
-    alert("No ha ingresado una opción de seguro válida.\nEl valor del envío es USD " + resultado + ". (Sin seguro) ")
+    alert("No ha ingresado una opción de seguro válida.\nEl valor del envío es USD " + resultadoEnvio + ". (Sin seguro) ")
 }
